b1c3b68fcfb034e0e62c7e0eefc8a0fd7323b456
Administrator <573087829@qq.com>
2020-07-15 19:01:59 +0800

    yangmou

---
+++ src/test/java/yhl/dbc/DbcApplicationTests.java
@@ -0,0 +1,13 @@
+package yhl.dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
